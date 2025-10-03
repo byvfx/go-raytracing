279fbe5481986dda56abe04a9427ac3a1a63f306
Brandon Young <brandon@byvfx.com>
2025-10-03 13:24:40 -0700

    Add PPM image generator with progress bar
    - Implemented a simple PPM image generator that creates a 256x256 image.
    - Added a progress bar to visualize the rendering progress in the console.
    - The image is generated using a gradient based on pixel position.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module go-raytracing
+
+go 1.25.1
---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func progressBar(done, total, width int) {
+	p := float64(done) / float64(total)
+	filled := int(p*float64(width) + 0.5)
+	if filled > width {
+		filled = width
+	}
+	bar := strings.Repeat("#", filled) + strings.Repeat(".", width-filled)
+	fmt.Fprintf(os.Stderr, "\r[%s] %3.0f%%  scanlines remaining: %d", bar, p*100, total-done)
+}
+
+func main() {
+
+	imageWidth := 256
+	imageHeight := 256
+	const barWidth = 40
+
+	out, err := os.Create("image.ppm")
+	if err != nil {
+		panic(err)
+	}
+	w := bufio.NewWriter(out)
+	defer w.Flush()
+
+	fmt.Fprintf(w, "P3\n%d %d\n255\n", imageWidth, imageHeight)
+
+	for j := 0; j < imageHeight; j++ {
+		for i := 0; i < imageWidth; i++ {
+			r := float64(i) / float64(imageWidth-1)
+			g := float64(j) / float64(imageHeight-1)
+			b := 0.0
+
+			ir := int(255.999 * r)
+			ig := int(255.999 * g)
+			ib := int(255.999 * b)
+			fmt.Fprintf(w, "%d %d %d\n", ir, ig, ib)
+		}
+		progressBar(j+1, imageHeight, barWidth)
+	}
+}
