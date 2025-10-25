f6396df60a867939397a3da074812b6c8fadb517
Brandon Young <67305844+byvfx@users.noreply.github.com>
2025-10-24 21:28:16 -0700

    Remove commented C++ code and improve readability in Hit function

--- rt/aabb.go
+++ rt/aabb.go
@@ -57,7 +57,6 @@ func (box AABB) Hit(r Ray, rayT Interval) bool {
 	for axis := 0; axis < 3; axis++ {
 		ax := box.AxisInterval(axis)
 
-		// Get the appropriate component based on axis
 		var axisOrig, axisDir float64
 		switch axis {
 		case 0:
@@ -71,21 +70,11 @@ func (box AABB) Hit(r Ray, rayT Interval) bool {
 			axisDir = rayDir.Z
 		}
 
-		// C++: const double adinv = 1.0 / ray_dir[axis];
 		adinv := 1.0 / axisDir
 
-		// C++: auto t0 = (ax.min - ray_orig[axis]) * adinv;
-		//      auto t1 = (ax.max - ray_orig[axis]) * adinv;
 		t0 := (ax.Min - axisOrig) * adinv
 		t1 := (ax.Max - axisOrig) * adinv
 
-		// C++: if (t0 < t1) {
-		//          if (t0 > ray_t.min) ray_t.min = t0;
-		//          if (t1 < ray_t.max) ray_t.max = t1;
-		//      } else {
-		//          if (t1 > ray_t.min) ray_t.min = t1;
-		//          if (t0 < ray_t.max) ray_t.max = t0;
-		//      }
 		if t0 < t1 {
 			if t0 > rayT.Min {
 				rayT.Min = t0
@@ -101,8 +90,6 @@ func (box AABB) Hit(r Ray, rayT Interval) bool {
 				rayT.Max = t0
 			}
 		}
-
-		// C++: if (ray_t.max <= ray_t.min) return false;
 		if rayT.Max <= rayT.Min {
 			return false
 		}
